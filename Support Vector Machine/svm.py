a62905564998f4bb00858c078ef2934de6839261
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-06-08 17:46:53 +0530

    Naive Bayes

--- Support Vector Machine/svm.py
+++ Support Vector Machine/svm.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     from sklearn.model_selection import train_test_split
     from sklearn import datasets
 
-    X, y = datasets.make_blobs(n_samples=50, n_features=2, centers=2, cluster_std=1.05, random_state=48)
+    X, y = datasets.make_blobs(n_samples=500, n_features=2, centers=2, cluster_std=1.05, random_state=48)
     y = np.where(y==0, -1, 1)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
